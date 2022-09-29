6cffe7156c241a975a155710ce6f9c5cc3c32656
Macey Bell <maceytbell@gmail.com>
2022-09-29 15:06:01 -0600

    Lab is finished

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -49,7 +49,6 @@ for(let i=0; i < nums.length; i++){
 }
 console.log(evensArr)
 
-
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
@@ -146,7 +145,24 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-
+switch (letterGrade){
+  case 'A':
+  console.log("The student is doing excellently.");
+  break;
+  case 'B':
+  console.log("The student is doing well.");
+  break;
+  case 'C':
+  console.log("The student is doing alright.");
+  break;
+  case 'D':
+  console.log("The student is not doing very well.");
+  break;
+  case 'F':
+  console.log('The student is failing.')
+  default :
+  console.log('Not an eligible grade')
+}
 
 
 ////////// PROBLEM 11 //////////
@@ -173,4 +189,15 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
   ...
 */
 
-//Code Here
+for(i = 1; i <= 100; i++){
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log('Devmountain')
+  } else if(i % 3 === 0){
+    console.log('Dev')
+  } else if(i % 5 === 0){
+    console.log('mountain')
+  } else {
+    console.log(i)
+  }
+  
+}
